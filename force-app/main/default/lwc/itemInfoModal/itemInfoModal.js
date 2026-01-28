0eff9f34d149e8eea8a513f72fa498d30fc55b86
Xramu <julius.eliel.liias@gmail.com>
2026-01-28 12:03:30 +0200

    Add: Scraping functionality to get the HTML bodies based on the product EAN code.

--- force-app/main/default/lwc/itemInfoModal/itemInfoModal.js
+++ force-app/main/default/lwc/itemInfoModal/itemInfoModal.js
@@ -1,20 +1,53 @@
-import { api } from 'lwc';
+import { api, track } from 'lwc';
 import LightningModal from 'lightning/modal';
+import getHtmlBodyFromUrl from '@salesforce/apex/EanScraperService.getHtmlBodyFromUrl';
 
 export default class ItemInfoModal extends LightningModal {
   @api item;
 
-  errorMessage = 'Tuotteen tietojen lataaminen epäonnistui';
-  get extraDataJson() {
-    const raw = this.item?.extraJsonData;
-    if (!raw) return this.errorMessage;
+  // Debug purposes the whole body of the product page.
+  @track productPageHtml = '';
 
+  // Domain start of each product.
+  productUrlStart = 'https://www.s-kaupat.fi/tuote/'
+
+  connectedCallback() {
+    this.loadProductPageHtml();
+  }
+
+  async loadProductPageHtml() {
+    if (!this.item) {
+      this.productPageHtml = 'Could not find product.';
+      return;
+    }
+
+    // Check for extra data json string
+    const raw = this.item?.extraJsonData;
+    if (!raw) {
+      this.productPageHtml = 'Could not find product extra data.';
+      return;
+    }
+
+    // Parse the extra data
+    var parsed = {};
     try {
-      return typeof raw === 'string' ? JSON.stringify(JSON.parse(raw)) : raw;
+      parsed = typeof raw === 'string' ? JSON.parse(raw) : raw;
     } catch (e) {
       console.log(e);
-      return this.errorMessage;
+      this.productPageHtml = 'Could not parse product extra data.';
+      return;
     }
+
+    getHtmlBodyFromUrl({ url: (this.productUrlStart + parsed.ean) })
+      .then((result) => {
+        this.productPageHtml = result;
+      })
+      .catch((e) => {
+        this.productPageHtml = e.body.message;
+        console.log(e);
+      })
+
+    this.productPageHtml = htmlBody;
   }
 
   get modalTitle() {
