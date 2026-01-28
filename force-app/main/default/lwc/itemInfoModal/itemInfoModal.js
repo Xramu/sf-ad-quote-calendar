6dc229f800f71a18648543e52e4983361441c5eb
Xramu <julius.eliel.liias@gmail.com>
2026-01-28 14:23:59 +0200

    Add: HTML parsing of the scraped website. Scraping happens on the client side.

--- force-app/main/default/lwc/itemInfoModal/itemInfoModal.js
+++ force-app/main/default/lwc/itemInfoModal/itemInfoModal.js
@@ -1,30 +1,30 @@
 import { api, track } from 'lwc';
 import LightningModal from 'lightning/modal';
+import getProductDataFromUrl from '@salesforce/apex/EanScraperService.getProductDataFromUrl';
 import getHtmlBodyFromUrl from '@salesforce/apex/EanScraperService.getHtmlBodyFromUrl';
 
 export default class ItemInfoModal extends LightningModal {
   @api item;
 
-  // Debug purposes the whole body of the product page.
-  @track productPageHtml = '';
+  @track productData;
+  @track isLoading = false;
+  @track hasError = false;
 
   // Domain start of each product.
   productUrlStart = 'https://www.s-kaupat.fi/tuote/'
 
   connectedCallback() {
-    this.loadProductPageHtml();
+    this.loadProductData();
   }
 
-  async loadProductPageHtml() {
+  async loadProductData() {
     if (!this.item) {
-      this.productPageHtml = 'Could not find product.';
       return;
     }
 
     // Check for extra data json string
     const raw = this.item?.extraJsonData;
     if (!raw) {
-      this.productPageHtml = 'Could not find product extra data.';
       return;
     }
 
@@ -34,20 +34,83 @@ export default class ItemInfoModal extends LightningModal {
       parsed = typeof raw === 'string' ? JSON.parse(raw) : raw;
     } catch (e) {
       console.log(e);
-      this.productPageHtml = 'Could not parse product extra data.';
       return;
     }
 
-    getHtmlBodyFromUrl({ url: (this.productUrlStart + parsed.ean) })
-      .then((result) => {
-        this.productPageHtml = result;
-      })
-      .catch((e) => {
-        this.productPageHtml = e.body.message;
-        console.log(e);
-      })
+    this.isLoading = true;
 
-    this.productPageHtml = htmlBody;
+    // Get html body from url and scrape it
+    await getHtmlBodyFromUrl({ url: (this.productUrlStart + parsed.ean) })
+    .then((result) => {
+      this.scrapeProductDataFromHtml(result);
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+
+    this.isLoading = false;
+  }
+
+  getDataTestIdElement(doc, dataTestId) {
+    return doc.querySelector('[data-test-id="' + dataTestId + '"]');
+  }
+
+  getProductNameFromHtmlBody(doc) {
+    const productTitleElement = this.getDataTestIdElement(doc, 'product-name');
+
+    if (!productTitleElement) {
+      return 'Nimeä ei voitu löytää.';
+    }
+
+    return productTitleElement.textContent;
+  }
+
+  getProductDescriptionFromHtmlBody(doc) {
+    const productDescriptionElement = this.getDataTestIdElement(doc, 'product-info-description').querySelector('div div span p');
+
+    if (!productDescriptionElement) {
+      return 'Kuvausta ei voitu löytää.'
+    }
+
+    return productDescriptionElement.textContent;
+  }
+
+  getProductCountryOfOriginFromHtmlBody(doc) {
+    const productCountryOfOriginElement = this.getDataTestIdElement(doc, 'product-info-country').querySelector('[class="sc-186ce282-2 bCfrWu"]');
+
+    if (!productCountryOfOriginElement) {
+      return 'Valmistusmaata ei voitu löytää.';
+    }
+
+    return productCountryOfOriginElement.textContent;
+  }
+
+  scrapeProductDataFromHtml(htmlString) {
+    const parser = new DOMParser();
+
+    const doc = parser.parseFromString(htmlString, 'text/html');
+
+    this.productData = {
+      productName: this.getProductNameFromHtmlBody(doc),
+      productDescription: this.getProductDescriptionFromHtmlBody(doc),
+      productCountryOfOrigin: this.getProductCountryOfOriginFromHtmlBody(doc),
+    };
+  }
+
+  get errorMessage() {
+    return this.productData?.errorMessage;
+  }
+
+  get productName() {
+    return this.productData?.productName;
+  }
+
+  get productDescription() {
+    return this.productData?.productDescription;
+  }
+
+  get productCountryOfOrigin() {
+    return this.productData?.productCountryOfOrigin;
   }
 
   get modalTitle() {
