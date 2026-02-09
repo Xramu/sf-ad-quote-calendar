694a76485477ce086096c411d908ba3778ab4fbf
Xramu <julius.eliel.liias@gmail.com>
2026-02-09 13:51:47 +0200

    Add: Simple Item View works now based on the new utils.

--- force-app/main/default/lwc/simpleItemInfoView/simpleItemInfoView.js
+++ force-app/main/default/lwc/simpleItemInfoView/simpleItemInfoView.js
@@ -1,6 +1,49 @@
-import { track, LightningElement } from 'lwc';
+import { track, api, LightningElement } from 'lwc';
+import { ProductDataManager } from 'c/itemInfoUtils';
 
 export default class SimpleItemInfoVIew extends LightningElement {
-  @track isLoading = false;
-  @track hasError = false;
+  @api eanCode = '';
+
+  @track productImageUrl = '';
+  @track productData = [];
+
+  @track dataManager = new ProductDataManager();
+
+  get formatter() {
+    return this.dataManager.formatter;
+  }
+
+  async connectedCallback() {
+    const success = await this.dataManager.fetchProductData(this.eanCode);
+    success ? this.onFetchSuccess() : this.onFetchFail();
+  }
+
+  onFetchFail() {
+    console.log('Failed fetch');
+  }
+
+  onFetchSuccess() {
+    this.displayProductData();
+  }
+
+  displayProductData() {
+    // Confirm data
+    if (!this.dataManager.hasData()) {
+      return;
+    }
+
+    // Data entries to show in simple view
+    this.productData = [
+      this.formatter.createNameEntry(),
+      this.formatter.createDescriptionEntry(),
+      this.formatter.createEanCodeEntry()
+    ].filter((entry) => !!entry);
+
+    // Image
+    this.productImageUrl = this.dataManager.getImageUrl360();
+  }
+
+  hasProductData() {
+    return this.productData.length > 0;
+  }
 }
