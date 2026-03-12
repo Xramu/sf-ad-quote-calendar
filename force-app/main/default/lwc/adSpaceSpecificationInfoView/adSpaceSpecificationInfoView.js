950a4882a29e7ec9faa0fe4ade942ea653c903a0
Xramu <julius.eliel.liias@gmail.com>
2026-03-12 13:58:43 +0200

    Add: Work on the adSpecifications info sets.

--- force-app/main/default/lwc/adSpaceSpecificationInfoView/adSpaceSpecificationInfoView.js
+++ force-app/main/default/lwc/adSpaceSpecificationInfoView/adSpaceSpecificationInfoView.js
@@ -1,9 +1,52 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
 export default class AdSpaceSpecificationInfoView extends LightningElement {
-  @api adSpaceSpecification;
+  _adSpaceSpecification;
+  @track infoSets = [];
 
-  get adSpaceSpecificationName() {
-    return this.adSpaceSpecification?.name ?? 'Nimeä ei löytynyt.';
+  @api
+  get adSpaceSpecification() {
+    return this._adSpaceSpecification;
+  }
+
+  set adSpaceSpecification(value) {
+    this._adSpaceSpecification = value;
+
+    if (!value) {
+      return;
+    }
+
+    // Reset info sets
+    this.infoSets = [];
+
+    this.addInfoSetPair('Nimi', this.adSpaceSpecification?.name);
+    // this.addInfoSetPair('Tyyppi', this.adSpaceSpecification?.type);
+    this.addInfoSetPair('Tila', this.getIsActiveValue(this.adSpaceSpecification?.isActive));
+    this.addInfoSetPair('Näyttökertojen Arvio', this.adSpaceSpecification?.audienceSizeRating);
+    this.addInfoSetPair('Tyyppi', this.adSpaceSpecification?.adSpaceType);
+    this.addInfoSetPair('Paikka', this.adSpaceSpecification?.position);
+    this.addInfoSetPair('Ohjelma', this.adSpaceSpecification?.programRunType);
+    this.addInfoSetPair('Tiedotusvälineen Tyyppi', this.adSpaceSpecification?.creativeFormatType);
+  }
+
+  getIsActiveValue(isActive) {
+    if (isActive == null) {
+      return null;
+    }
+
+    return isActive ? 'Aktiivinen' : 'Ei Aktiivinen'
+  }
+
+  addInfoSetPair(label, value) {
+    // Return null that will be filtered out
+    if (!label || !value) {
+      return null;
+    }
+
+    this.infoSets.push({ label, value })
+  }
+
+  get hasInfoSets() {
+    return this.infoSets?.length > 0;
   }
 }
--- force-app/main/default/lwc/itemInfoField/itemInfoField.js
+++ force-app/main/default/lwc/itemInfoField/itemInfoField.js
@@ -3,8 +3,4 @@ import { LightningElement, api } from 'lwc';
 export default class ItemInfoField extends LightningElement {
     @api label;
     @api value;
-
-    get displayValue() {
-        return this.value ?? '-';
-    }
 }
