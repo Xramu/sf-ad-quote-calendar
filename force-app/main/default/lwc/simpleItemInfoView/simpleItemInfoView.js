39e0e18845454ad30e6677fc7ac580c131c79aeb
Xramu <julius.eliel.liias@gmail.com>
2026-04-09 12:33:59 +0300

    Add: Translation support to simpleItemInfoView and its array components.

--- force-app/main/default/lwc/simpleItemInfoView/simpleItemInfoView.js
+++ force-app/main/default/lwc/simpleItemInfoView/simpleItemInfoView.js
@@ -4,6 +4,7 @@ export default class SimpleItemInfoVIew extends LightningElement {
   @api eanCode;
   @api imageWidth;
   @api imageHeight;
+  @api inEnglish = false;
 
   get detailsToShow() {
     return 'Image, Name, Description, EAN Code';
--- force-app/main/default/lwc/simpleItemInfoViewArray/simpleItemInfoViewArray.js
+++ force-app/main/default/lwc/simpleItemInfoViewArray/simpleItemInfoViewArray.js
@@ -4,4 +4,5 @@ export default class SimpleItemInfoViewArray extends LightningElement {
     @api eanCodes = [];
     @api imageWidth;
     @api imageHeight;
+    @api inEnglish = false;
 }
